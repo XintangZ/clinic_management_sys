20c9d0549a9c7b522a350d44d0cf2fbd7b9c130e
Xintang ZHANG <xtzhang1994@gmail.com>
2023-12-22 12:42:28 -0500

    updated test

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,7 +5,7 @@ import src.main.Gender;
 
 public class Test {
     public static void main(String[] args) {
-        Doctor doctor = new Doctor("John", "Smith", "2000-12-31", Gender.M, "123-456-7890", "123 Cat St.",
+        Doctor doctor = new Doctor("John", "Smith", "2000-12-19", Gender.M, "123-456-7890", "123 Cat St.",
                 "2020-01-01", "General");
         System.out.println(doctor);
     }
