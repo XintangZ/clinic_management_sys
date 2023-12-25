851ad764ef0c14762f69d1106fb4b5f2fb72defe
Leah Yapp <leahyapp@gmail.com>
2023-12-25 13:28:14 -0500

    creation of appointment and receptionist class

--- src/main/Appointment.java
+++ src/main/Appointment.java
@@ -1,4 +1,6 @@
 package src.main;
+import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * Class Appointment
@@ -9,5 +11,44 @@ package src.main;
  */
 
 public class Appointment {
+    private Date appointmentStartTime;  // format: mm/dd/yyyy hh:mm
+    private Date appointmentEndTime;    // format: mm/dd/yyyy hh:mm
+    private String patientName;
+    private String doctorName;
+    private String status;
     
+
+    public Appointment(Date appointmentStart, Date appointmentEnd, String patientName, String doctorName){
+        setAppointment(appointmentStart, appointmentEnd, patientName, doctorName);
+    }
+    
+    public void setAppointment(Date appointmentStartTime, Date appointmentEndTime, String patientName, String doctorName) {   // same as edit appointment
+        this.appointmentStartTime = appointmentStartTime;
+        this.appointmentEndTime = appointmentEndTime;
+        this.patientName = patientName;
+        this.doctorName = doctorName;
+    } // receptionist has access to this method
+
+    public String getDoctorName() {
+        return this.doctorName;
+    }
+
+    public String getPatientName() {
+        return this.patientName;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public ArrayList<Appointment> getAppointmentList(ArrayList<Appointment> appointments) {
+        ArrayList<Appointment> upcomingAppointments = new ArrayList<>();
+        for (Appointment appointment: appointments) {
+            if (!appointment.getStatus().equals("Complete") || !appointment.getStatus().equals("Cancelled")) {
+                upcomingAppointments.add(appointment);
+            }
+        }
+        return upcomingAppointments;
+    }
+
 }
--- src/main/Receptionist.java
+++ src/main/Receptionist.java
@@ -1,4 +1,5 @@
 package src.main;
+import java.util.*;
 
 /**
  * Class Receptionist extends the abstract class Employee
@@ -8,6 +9,50 @@ package src.main;
  * @author Team 6
  */
 
-public class Receptionist extends Employee{
-    
+public class Receptionist extends Employee {
+    private double balanceDue;
+
+    public Receptionist(String firstName, String lastName, String dateOfBirth, Gender gender, String phoneNumber,
+    String address, String dateOfEmployment) {
+        super(firstName, lastName, dateOfBirth, gender, phoneNumber, address, dateOfEmployment);
+    }
+
+    public void updatePatientInfo(Patient patient) {   // create patient would be constructor in patient class
+        Patient.setPatient(patient); 
+    } // patient class
+
+    public double getPatientBalance() {
+        return this.balanceDue;
+    } // patient class
+
+    public double chargePatient(double charge) { 
+        return Patient.getPatientBalance() + charge;
+    }
+
+    public double applyPayment(double payment) {
+        return Patient.getPatientBalance() - payment;
+    }
+
+    // returns a list of all the doctor's upcoming appointments
+    public ArrayList<Appointment> getDoctorScehdule(String doctorName, ArrayList<Appointment> appointmentList) {
+        ArrayList<Appointment> filteredList = new ArrayList<>();
+        for (Appointment appointment : appointmentList) {
+            if (appointment.getDoctorName().equals(doctorName)) {
+                filteredList.add(appointment);
+            }
+        }
+        return filteredList;
+    }
+
+    // returns a list of all the patient's upcoming appointments
+    public ArrayList<Appointment> getPatientAppointments(String patientName, ArrayList<Appointment> appointmentList) {
+        ArrayList<Appointment> filteredList = new ArrayList<>();
+        for (Appointment appointment : appointmentList) {
+            if (appointment.getPatientName().equals(patientName)) {
+                filteredList.add(appointment);
+            }
+        }
+        return filteredList;
+    }
+
 }
