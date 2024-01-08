85e70fe98d49fa2b7d48077c8a28dc5791e27f62
Xintang ZHANG <xtzhang1994@gmail.com>
2024-01-08 15:56:50 -0500

    removed methods to new files

--- src/utils/UserInteraction.java
+++ src/utils/UserInteraction.java
@@ -1,9 +1,8 @@
 package src.utils;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.time.format.TextStyle;
 import java.time.temporal.TemporalAdjusters;
@@ -13,9 +12,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import src.clinic.Treatment;
-import src.person.BloodType;
 import src.person.Doctor;
-import src.person.Gender;
 import src.person.Patient;
 import src.person.Person;
 import src.clinic.Appointment;
@@ -32,107 +29,6 @@ import src.clinic.Appointment;
 
 public interface UserInteraction {
     /**
-     * runs a method for a specified number of times
-     * 
-     * @param method a method to run for a certain number of times
-     * @param attempts an int for the number of times the method will run
-     */
-    public static void limitAttempts(RunnableWithException methodToRun, int attempts) throws Exception {
-        do {
-            try {
-                methodToRun.run();
-                return;
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                if (attempts > 2) {
-                    System.out.printf("%d attempts left. %n", --attempts);
-                } else {
-                    System.out.printf("%d attempt left. %n", --attempts);
-                }
-            }
-        } while (attempts > 0);
-        throw new Exception("Maximum attempts exceeded, session ends.");
-    } // end method limitAttempts
-
-    /**
-     * displays a message in the ternimal
-     * and prompts the user to enter a non-empty String
-     *  
-     * @param scanner a Scanner object to read user input
-     * @param promptMsg a String of the prompt message to be displayed to the user
-     * @return a String entered by the user
-     */
-    public static String promptForString(Scanner scanner, String promptMsg) {
-        String str;
-        while (true) {
-            System.out.print(promptMsg + ": ");
-            try {
-                str = scanner.nextLine().trim();
-                if (str.isBlank()) {
-                    throw new Exception(String.format("Invalid input. %s cannot be empty.", promptMsg));
-                }
-                return str;
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
-    } // end method prompForString
-
-    /**
-     * displays a message in the ternimal
-     * and prompts the user to enter a non-negative number
-     *  
-     * @param scanner a Scanner object to read user input
-     * @param promptMsg a String of the prompt message to be displayed to the user
-     * @return an int entered by the user
-     */
-    public static int promptForPositiveInt(Scanner scanner, String promptMsg) {
-        int num;
-        while (true) {
-            System.out.print(promptMsg + ": ");
-            try {
-                num = scanner.nextInt();
-                // check if the user input is greater than 0
-                if (num < 0) {
-                    throw new Exception(String.format("Invalid input. %s cannot be negative.", promptMsg));
-                } // end if
-                return num;
-            } // end try
-            catch (InputMismatchException e) {
-                scanner.nextLine();
-                System.err.println((String.format("Invalid input. %s must be a whole number.", promptMsg)));
-            }  // end catch InputMismatchException
-            catch (Exception e) {
-                scanner.nextLine();
-                System.err.println(e.getMessage());
-            } // end catch Exception
-        } // end while loop
-    } // end method promptForPositiveInt
-
-    /**
-     * displays a message in the ternimal
-     * and prompts the user to enter a date in format yyyy-mm-dd,
-     * then parses the user input to a LocalDate object
-     *  
-     * @param scanner a Scanner object to read user input
-     * @param promptMsg a String of the prompt message to be displayed to the user
-     * @return a localDate object parsed from the String entered by the user
-     */
-    public static LocalDate promptForDate(Scanner scanner, String promptMsg) {
-        String dateToParse;
-        LocalDate date;
-        while (true) {
-            dateToParse = promptForString(scanner, promptMsg);
-            try {
-                date = LocalDate.parse(dateToParse);
-                return date;
-            } catch (DateTimeParseException e) {
-                System.err.println("Invalid date format. Format must be \"yyyy-mm-dd\".");
-            }
-        }
-    } // end method promptForDate
-
-    /**
      * displays a message in the ternimal
      * and prompts the user to enter a time in format hh:mm,
      * then parses the user input to a LocalTime object
@@ -141,7 +37,7 @@ public interface UserInteraction {
      * @param promptMsg a String of the prompt message to be displayed to the user
      * @return a localTime object parsed from the String entered by the user
      */
-    public static LocalTime promptForTime(Scanner scanner) {
+    static LocalTime promptForTime(Scanner scanner) {
         while (true) {
             int hour = promptForPositiveInt(scanner, "Enter the starting time in the format hh mm: ");
             String str = Integer.toString(hour);
@@ -158,15 +54,8 @@ public interface UserInteraction {
         }
     } // end method promptForTime
 
-    /**
-     * displays a menu with numeric options in the terminal
-     * and prompts the user to choose one of the menu option by enterning a number
-     * 
-     * @param scanner a Scanner object to read user input
-     * @param menu an ArrayList of Strings containing menu options to be displayed to the user
-     * @return the int entered by the user
-     */
-    public static int chooseFromMenu(Scanner scanner, String[] menu) {
+    // deprecated. use Menu class instead
+    static int chooseFromMenu(Scanner scanner, String[] menu) {
 		boolean isInvalidChoice = true;
         String input;
 
@@ -191,171 +80,7 @@ public interface UserInteraction {
 
         return Integer.parseInt(input);
     } // end method chooseFromMenu
-
-    /**
-     * prompts the user a closed question (yes/no question) <p>
-     * returns true if the user inputs "y" or "yes" <p>
-     * returns false if the uesr inputs "n" or "no" <p>
-     * (Cases are ignored.)
-     * 
-     * @param closedQuestion a String of a closed question
-     * @return a boolean, the value of which is determined by the user input
-     */
-    public static boolean promptForResponse(Scanner scanner, String closedQuestion) {
-        boolean response;
-        while (true) {
-            try {
-                // print the question
-                System.out.println(closedQuestion + " (y/n)");
-                // format user input
-                String userInput = scanner.nextLine().trim().toLowerCase();
-
-                if (userInput.equals("y") || userInput.equals("yes")) {
-                    response = true;
-                } else if (userInput.equals("n") || userInput.equals("no")) {
-                    response = false;
-                } else {
-                    throw new Exception("Invalid response. Please try again.");
-                } // end if
-                return response;
-            } // end try
-            catch (Exception e) {
-                scanner.nextLine();
-                System.err.println(e.getMessage());
-            } // end catch Exception
-        } // end while loop
-    } // end method promptForResponse
-
-    /**
-     * instantiates a Doctor object
-     * by prompting the user to enter attributes in the terminal
-     * 
-     * @param scanner a Scanner object to read user input
-     * @return a Doctor object
-     */
-    public static Doctor createDoctor(Scanner scanner) {
-        Doctor doctor = new Doctor();
-
-        setPersonalInfo(scanner, doctor);
-
-        while (true) {
-            try {
-                doctor.setDateOfEmployment(promptForDate(scanner, "Date of Employment"));
-                break;
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
-        doctor.setSpecialty(promptForString(scanner, "Specialty"));
-
-        return doctor;
-    } // end method createDoctor
-
-    /**
-     * instantiates a Patient object
-     * by prompting the user to enter attributes in the terminal
-     * 
-     * @param scanner a Scanner object to read user input
-     * @return a Patient object
-     */
-    public static Patient createPatient(Scanner scanner) {
-        // BloodType bloodType;
-        Patient patient = new Patient();
-
-        setPersonalInfo(scanner, patient);
-        patient.setAllergies(promptForString(scanner, "Allergies"));
-        patient.setInsuranceCompany(promptForString(scanner, "Insurance Company"));
-        patient.setPolicyNumber(promptForString(scanner, "Policy Number"));
-        patient.setCoveredPercentage(promptForPositiveInt(scanner, "Covered Percentage"));
-
-        // bloodType = promptForString("Blood Type");
-        // patient.setBloodType(bloodType);
-
-        return patient;
-    } // end method createPatient 
     
-    /**
-     * initializes each property of a Person object
-     * by prompting the user to enter attributes in the terminal
-     * 
-     * @param scanner a Scanner object to read user input
-     * @param person a Person object to add attributes to
-     */
-    public static void setPersonalInfo(Scanner scanner, Person person) {
-        person.setFirstName(promptForString(scanner, "First Name"));
-        person.setLastName(promptForString(scanner, "Last Name"));
-
-        while (true) {
-            try {
-                person.setDateOfBirth(promptForDate(scanner, "Date of Birth"));
-                break;
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
-        
-        while (true) {
-            try {
-                person.setGender(promptForString(scanner, "Gender"));
-                break;
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
-        person.setPhoneNumber(promptForString(scanner, "Phone Number"));
-        person.setAddress(promptForString(scanner, "Address"));
-    } // end method setPersonalInfo
-    
-    /**
-     * instantiates a Treatment object
-     * by prompting the user to enter attributes in the terminal
-     * 
-     * @param scanner a Scanner object to read user input
-     * @return a Treatment object
-     */
-    static Treatment createTreatment(Scanner scanner) {
-        Treatment treatment = new Treatment();
-
-        treatment.setMedication(promptForString(scanner, "Medication"));
-        treatment.setDescription(promptForString(scanner, "Description"));
-
-        while (true) {
-            try {
-                treatment.setStartDate(promptForDate(scanner, "Start date"));
-                break;
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
-
-        while (true) {
-            try {
-                treatment.setEndDate(promptForDate(scanner, "End date"));
-                break;
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
-
-        return treatment;
-    } // end method createTreatment
-
-    static Object searchForPerson(Scanner scanner, ArrayList<Object> arrayList) {
-        String firstName, lastName, name, phoneNumber;
-
-        firstName = promptForString(scanner, "First Name");
-        lastName = promptForString(scanner, "Last Name");
-        name = firstName + " " + lastName;
-
-        phoneNumber = promptForString(scanner, "Phone Number");
-
-        for (Object obj : arrayList) {
-            if (((Person) obj).getName().equals(name) && ((Person) obj).getPhoneNumber().equals(phoneNumber)) {
-                return obj;
-            }
-        } 
-        return null;
-    }
 
     static ArrayList<Object> searchForAppointments(Scanner scanner, ArrayList<Object> arrayList) {
         
@@ -382,7 +107,7 @@ public interface UserInteraction {
                 } 
                 break;
             case 3:
-                LocalDate date = promptForDate(scanner, "Appointment Date");
+                LocalDate date = promptForString(scanner, "Appointment Date");
                 for (Object obj : filter) {
                     if (((Appointment) obj).getDate().equals(date)) {
                         filter.add(obj);
@@ -390,7 +115,7 @@ public interface UserInteraction {
                 }
                 break;
             default:
-                LocalDate specificDate = promptForDate(scanner, "Appointment Date");
+                LocalDate specificDate = promptForString(scanner, "Appointment Date");
                 LocalTime specificTime =  promptForTime(scanner);
                 scanner.nextLine();
                 String specificDoctorName = promptForString(scanner, "Doctor's Name");
@@ -405,7 +130,6 @@ public interface UserInteraction {
     }
     
     public static Appointment createAppointment(Scanner scanner, String patientName) {
-        LocalDate appointmentDate;
         LocalTime startTime, endTime;
         String doctorName;
         int minutes;
@@ -424,9 +148,8 @@ public interface UserInteraction {
         }
         showDoctorAvailabilitySchedule(doctorName, filter);
         while (true) {
-            appointmentDate = promptForDate(scanner, "Appointment Date");
             try {
-                appointment.setDate(appointmentDate);
+                appointment.setDate(promptForString(scanner, "Appointment Date"));
                 break;
             } catch (Exception e) {
                 System.err.println(e.getMessage());
@@ -450,28 +173,31 @@ public interface UserInteraction {
         }
         LocalDate today = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.SATURDAY));
         today.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
-        String[] days = {   "Mon (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY)) + ")",
-                            "Tue (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.TUESDAY))+ ")",
-                            "Wed (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.WEDNESDAY))+ ")",
-                            "Thu (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.THURSDAY))+ ")",
-                            "Fri (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY))+ ")"};
-        
-        for(int i = 0; i < days.length; i++) {
+        String[] days = { "Mon (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY)) + ")",
+                "Tue (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.TUESDAY)) + ")",
+                "Wed (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.WEDNESDAY)) + ")",
+                "Thu (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.THURSDAY)) + ")",
+                "Fri (" + today.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY)) + ")" };
+
+        for (int i = 0; i < days.length; i++) {
             System.out.print(days[i] + "\t");
             String[] times = { "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "13:00", "13:30", "14:00", "14:30",
-                "15:00", "15:30" };
+                    "15:00", "15:30" };
             for (int j = 0; j < times.length; j++) {
                 for (Object obj : filter) {
-                    if (((Appointment) obj).getDate().getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.ENGLISH).equals(days[i].substring(0, 3))) {
-                        if ((((Appointment) obj).getStartTime().isBefore(LocalTime.parse(times[j])) || (((Appointment) obj).getStartTime()).equals(LocalTime.parse(times[j])))
-                        && ((((Appointment) obj).getEndTime()).equals(LocalTime.parse(times[j])) || (((Appointment) obj).getEndTime()).isAfter(LocalTime.parse(times[j])))) {
+                    if (((Appointment) obj).getDate().getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.ENGLISH)
+                            .equals(days[i].substring(0, 3))) {
+                        if ((((Appointment) obj).getStartTime().isBefore(LocalTime.parse(times[j]))
+                                || (((Appointment) obj).getStartTime()).equals(LocalTime.parse(times[j])))
+                                && ((((Appointment) obj).getEndTime()).equals(LocalTime.parse(times[j]))
+                                        || (((Appointment) obj).getEndTime()).isAfter(LocalTime.parse(times[j])))) {
                             times[j] = "     ";
                         }
                     }
-                } 
+                }
             }
             for (int timesCounter = 0; timesCounter < times.length; timesCounter++) {
-                System.out.print(times[timesCounter] + "    "); 
+                System.out.print(times[timesCounter] + "    ");
             }
             System.out.println();
         }
