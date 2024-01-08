4facaa1f2dbb11fe2055a69601223ded76d4e765
Xintang ZHANG <xtzhang1994@gmail.com>
2024-01-08 15:55:25 -0500

    refined

--- src/main/DoctorPage.java
+++ src/main/DoctorPage.java
@@ -1,57 +1,138 @@
 package src.main;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import src.clinic.Treatment;
+import src.person.Patient;
 import src.utils.Menu;
 import src.utils.ObjectIO;
-import src.utils.UserInteraction;
+
+/**
+ * class DoctorPage extends Team6MedicalClinic
+ * contains the main method to run the DoctorPage
+ * 
+ * @version 1.00
+ * @since 2024-01-04
+ * @author Team 6
+ */
 
 public class DoctorPage extends Team6MedicalClinic {
-    private static Menu doctorMenu = new Menu("doctor", "New treatment", "Search for a treatment", "All treatments",
+    private static Menu doctorMenu = new Menu("doctor", "Create new treatment", "Search for a treatment", "All treatments",
             "Search for a patient", "All patients", "Main menu");
-    private static ArrayList<Object> patients = ObjectIO.loadData(ObjectIO.PATIENT_FILE_PATH);
-    private static ArrayList<Object> treatments = ObjectIO.loadData(ObjectIO.TREATMENT_FILE_PATH);
-
+    private static ArrayList<Object> patientList = ObjectIO.loadData(ObjectIO.PATIENT_FILE_PATH);
+    private static ArrayList<Object> treatmentList = ObjectIO.loadData(ObjectIO.TREATMENT_FILE_PATH);
 
     public static void main(String[] args) {
-
-        doctorMenu.execute(scanner, "Are you sure to leave this page?", createTreatment, searchTreatment,
-                displayAllTreatments, searchPatient, displayAllPatients);
-
+        // execute doctor menu
+        doctorMenu.execute("Are you sure to leave this page?", createTreatment, searchTreatments,
+                displayAllTreatments, searchPatients, displayAllPatients);
+        // quit message
         System.out.println("Leaving...");
     }
-    
+
+    // runnables to be passed as params
+    // create treatment
     private static Runnable createTreatment = () -> {
         System.out.println("======= NEW TREATMENT =======");
-        Treatment newTreatment = UserInteraction.createTreatment(scanner);
-        treatments.add(newTreatment);
-        ObjectIO.writeObjects(ObjectIO.TREATMENT_FILE_PATH, treatments);
+        Treatment newTreatment;
+        try {
+            newTreatment = user.createTreatment();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+        treatmentList.add(newTreatment);
+        ObjectIO.writeObjects(ObjectIO.TREATMENT_FILE_PATH, treatmentList);
     };
 
-    private static Runnable searchTreatment = () -> {
+    // search treatments
+    // TODO: modify searching algorithm
+    private static Runnable searchTreatments = () -> {
+        Treatment result;
+        try {
+            result = (Treatment) user.searchForTreatment(treatmentList);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+        
+        if (result == null) {
+            System.out.println("Treatment not found.");
+            return; // return if no result found
+        }
+        
+        System.out.println("======= SEARCH RESULT =======");
+        System.out.println(result); // print the result
 
-    };
-
-    private static Runnable displayAllTreatments = () -> {
-        if (treatments.isEmpty()) {
-            System.out.println("No record.");
-        } else {
-            System.out.println("======= ALL TREATMENTS =======");
-            int index = 1;
-            for (Object obj : treatments) {
-                System.out.printf("------- Treatment %d ------- %n", index++);
-                System.out.println((Treatment) obj);
+        boolean[] isToEdit = new boolean[1];
+        try {
+            user.limitAttempts(() -> {
+                isToEdit[0] = user.getResponse(
+                        "Would you like to edit the treatment?");  // ask if the user wants to edit the found object
+            }, 3);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+        
+        if (isToEdit[0]) { 
+            try {
+                user.editTreatment(result); // edit object attributes
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return;
             }
+            ObjectIO.writeObjects(ObjectIO.TREATMENT_FILE_PATH, treatmentList); // write objects to data file
         }
     };
 
-    private static Runnable searchPatient = () -> {
-
+    // display all treatments
+    private static Runnable displayAllTreatments = () -> {
+        user.printAll(treatmentList, Treatment.class);
     };
 
-    private static Runnable displayAllPatients = () -> {
+    // search patients
+    public static Runnable searchPatients = () -> {
+        Patient result;
+        try {
+            result = (Patient) user.searchForPerson(patientList);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+        
+        if (result == null) {
+            System.out.println("Patient not found.");
+            return; // return if no result found
+        }
+        
+        System.out.println("======= SEARCH RESULT =======");
+        System.out.println(result); // print the result
 
+        boolean[] isToEdit = new boolean[1];
+        try {
+            user.limitAttempts(() -> {
+                isToEdit[0] = user.getResponse(
+                        "Would you like to edit the doctor's information?");  // ask if the user wants to edit the found object
+            }, 3);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return;
+        }
+        
+        if (isToEdit[0]) { 
+            try {
+                user.editPatient(result); // edit object attributes
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+            ObjectIO.writeObjects(ObjectIO.PATIENT_FILE_PATH, patientList); // write objects to data file
+        }
+    };
+    
+    // display all patients
+    public static Runnable displayAllPatients = () -> {
+        user.printAll(patientList, Patient.class);
     };
 }
--- src/main/ReceptionPage.java
+++ src/main/ReceptionPage.java
@@ -7,48 +7,67 @@ import java.util.ArrayList;
 
 import src.person.Doctor;
 import src.person.Patient;
-import src.utils.BusinessHours;
 import src.utils.Menu;
 import src.utils.ObjectIO;
 import src.utils.UserInteraction;
 import src.clinic.Appointment;
+import src.clinic.BusinessHours;
+
+/**
+ * class ReceptionPage extends Team6MedicalClinic
+ * contains the main method to run the ReceptionPage
+ * 
+ * @version 1.00
+ * @since 2024-01-04
+ * @author Team 6
+ */
 
 public class ReceptionPage extends Team6MedicalClinic {
-    private static Menu receptionMenu = new Menu("reception", "Display Schedule", "Create new appointment",
-            "Search for an appointment", "All appointments","Register new patient", "Search for a patient", "All patients", "Main menu");
+    private static Menu receptionMenu = new Menu("reception", "Create new appointment",
+            "Search for an appointment", "All appointments", "Register new patient", "Search for a patient", "All patients", "Main menu");
     private static ArrayList<Object> patientList = ObjectIO.loadData(ObjectIO.PATIENT_FILE_PATH);
     private static ArrayList<Object> appointmentList = ObjectIO.loadData(ObjectIO.APPOINTMENT_FILE_PATH);
     private static ArrayList<Object> doctorList = ObjectIO.loadData(ObjectIO.DOCTOR_FILE_PATH);
 
     public static void main(String[] args) {
+        // all incoming appointments
+        ArrayList<Object> futureAppointment = getFutureAppointments(appointmentList);
         // execute reception menu
-        receptionMenu.execute(scanner, "Are you sure to leave this page?", displaySchedule, createAppointment,
+        receptionMenu.execute("Are you sure to leave this page?", createAppointment,
                 searchAppointments, displayAllAppointments, registerPatient, DoctorPage.searchPatients, DoctorPage.displayAllPatients);
         // quit message
         System.out.println("Leaving...");
     } // end method main
     
     // runnables to be passed as params
-    // display schedule (14 days from tomorrow) 
-    private static Runnable displaySchedule = () -> {
-        LocalDate tomorrow = LocalDate.now().plusDays(1);
-        printSchedule(tomorrow, tomorrow.plusDays(14));
-    };
-
     // create appointment
     private static Runnable createAppointment = () -> {
+        LocalDate scheduleStartDate = LocalDate.now().plusDays(1);
+        LocalDate scheduleEndDate = scheduleStartDate.plusDays(13);
+
+        // TODO:
+        // ask for specialty (call getSpecialtyDoctors
+        // select doctor (doctor = chooseFromMenu
+        // display doctor schedule (printSchedule
+        printSchedule(scheduleStartDate, scheduleEndDate, null);
+        // ask if to create an appointment
+        // ask if the patient is new
+        // search for patient || create new patient
+        // select date
+        // select time
+        // enter the rest of attrs for the appointment
         String patientName = "";
         boolean noPatientDetails = true;
         while (noPatientDetails) {
-            if (UserInteraction.promptForResponse(scanner, "Is the person a new patient?")) {
+            if (user.getResponse("Is the person a new patient?")) {
                 System.out.println("\n======= NEW PATIENT =======");
-                Patient patient = UserInteraction.createPatient(scanner);
+                Patient patient = user.createPatient();
                 patientList.add(patient);
                 patientName = patient.getName();
                 noPatientDetails = false;
                 // ObjectIO.writeObjects(ObjectIO.PATIENT_FILE_PATH, patients);
             } else {
-                Object patient = UserInteraction.searchForPerson(scanner, patientList);
+                Object patient = user.searchForPerson(patientList);
                 if (patient == null) {
                     System.out.println("\nThis person does not exist.");
                 } else {
@@ -79,25 +98,23 @@ public class ReceptionPage extends Team6MedicalClinic {
         if (smallerFilter.size() == 0) {
             System.out.println("\nThere are no appointments that matches the criteria.");
         } else if (smallerFilter.size() == 1){
-            if (UserInteraction.promptForResponse(scanner, "There is only one appointment. Do you want to cancel it?")) {
+            if (user.getResponse("There is only one appointment. Do you want to cancel it?")) {
                 ((Appointment)smallerFilter.get(0)).setStatus("Cancelled");
                 System.out.println("======= APPOINTMENT CANCELLED =======");
                 System.out.println((Appointment)smallerFilter.get(0));
             }
-
         } else {
             for (Object obj : smallerFilter) {
                 System.out.println((Appointment) obj);
             }
         }
-
     };
 
     // display all appointments
     private static Runnable displayAllAppointments = () -> {
-        UserInteraction.printAll(appointmentList, Appointment.class);
+        user.printAll(appointmentList, Appointment.class);
     };
-    
+
     // register new patient
     private static Runnable registerPatient = () -> {
         System.out.println("======= NEW PATIENT =======");
@@ -114,7 +131,7 @@ public class ReceptionPage extends Team6MedicalClinic {
 
     // static methods
     /**
-     * prints a time table of a day 
+     * recursively prints a time table of a day.
      * each time slot is of 30-min length, 
      * the time table will be from 9am to 14pm, 
      * with a lunchbreak from noon to 1pm
@@ -122,107 +139,122 @@ public class ReceptionPage extends Team6MedicalClinic {
      * @param date a LocalDate of the current day
      * @param time a LocalTime when the time table begins
      */
-    static void printDailySchedule(LocalDate date, LocalTime time) {
-        if (time.equals(BusinessHours.CLOSE_TIME_PM.getTime())) {
-            System.out.println();
+    static void printDailySchedule(LocalDate date, LocalTime time, ArrayList<Appointment> doctorAppointments) {
+        if (time.equals(BusinessHours.CLOSE_TIME_PM.getTime())) {   // base case
+            System.out.println("|");
             return;
         }
 
         if (time.equals(BusinessHours.CLOSE_TIME_AM.getTime())) {
             System.out.print("\t");
-            printDailySchedule(date, BusinessHours.OPEN_TIME_PM.getTime());
+            printDailySchedule(date, BusinessHours.OPEN_TIME_PM.getTime(), doctorAppointments); // skip lunch break
         } else {
-            if (checkTimeSlotAvailability(date, time)) {
-                System.out.printf("%s\t", time);
-                printDailySchedule(date, time.plusMinutes(30));
+            if (checkAvailability(doctorAppointments, date, time)) {
+                System.out.printf("%s\t", time);    // if the doctor is available, print the time slot
+                printDailySchedule(date, time.plusMinutes(60), doctorAppointments);
             } else {
-                System.out.printf("\t\t");
-                printDailySchedule(date, time.plusMinutes(30));
+                System.out.printf("\t\t");      // if the doctor is not available, print white spaces only
+                printDailySchedule(date, time.plusMinutes(60), doctorAppointments);
             }
         }
     } // end method printDailySchedule
 
     /**
-     * prints a schedule of a specified range of days
+     * recursively prints a schedule of a specified range of days
      * 
      * @param startDate a LocalDate of the date the schedule begins
      * @param endDate a LocalDate of the date the schedule ends
      */
-    static void printSchedule(LocalDate startDate, LocalDate endDate) {
-        if (startDate.isAfter(endDate)) {
+    static void printSchedule(LocalDate startDate, LocalDate endDate, ArrayList<Appointment> doctorAppointments) {
+        if (startDate.isAfter(endDate)) {   // base case
             return;
         }
 
-        if (startDate.getDayOfWeek().equals(DayOfWeek.SATURDAY) || startDate.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
-            System.out.println();
-            printSchedule(startDate.plusDays(1), endDate);
+        if (startDate.getDayOfWeek().equals(DayOfWeek.SATURDAY) || startDate.getDayOfWeek().equals(DayOfWeek.SUNDAY)) { // skip weekends
+            System.out.println("|");
+            printSchedule(startDate.plusDays(1), endDate, doctorAppointments);
         } else {
-            System.out.printf("%s (%s)\t", startDate.getDayOfWeek().toString().substring(0, 3), startDate);
-            printDailySchedule(startDate, BusinessHours.OPEN_TIME_AM.getTime());
-            printSchedule(startDate.plusDays(1), endDate);
+            System.out.printf("| %s (%s)\t", startDate.getDayOfWeek().toString().substring(0, 3), startDate);     // print day of the week and date
+            printDailySchedule(startDate, BusinessHours.OPEN_TIME_AM.getTime(), doctorAppointments);    // print the time slots for the day
+            printSchedule(startDate.plusDays(1), endDate, doctorAppointments);
         }
     } // end method printSchedule
 
     /**
-     * finds all appointments of a certain date and time
+     * gets all doctors of a certain specialty
      * 
-     * @param date a LocalDate of the appointment date
-     * @param time a LocalTime of the appointment time
-     * @return an ArrayList of Appointments that matches the date and time
+     * @param doctorList an ArrayList of Object containing all doctors
+     * @param specialty a String specifying the specialty
+     * @return  an ArrayList of doctor of the specified specialty
      */
-    static ArrayList<Appointment> getAppointmentsOfSameTime(LocalDate date, LocalTime time) {
-        ArrayList<Appointment> bookedAppointments = new ArrayList<>();
-
-        for (Object obj : appointmentList) {
-            Appointment appointment = (Appointment) obj;
-            if (appointment.getDate().equals(date) && appointment.getStartTime().equals(time)
-                    && appointment.getStatus().equals("Comfirmed")) {
-                bookedAppointments.add(appointment);
-            }
-        }
-
-        return bookedAppointments;
-    } // end method getAppointmentOfSameTime
-
-    /**
-     * checks if a doctor is available for a certain time slot
-     * 
-     * @param doctor a Doctor to check
-     * @param bookedAppointments an ArrayList of Appointments of the same date and time
-     * @return a boolean indicating if the doctor is available
-     */
-    static boolean checkDoctorAvailability(Doctor doctor, ArrayList<Appointment> bookedAppointments) {
-        boolean isAvailable = true;
-
-        for (Appointment appointment : bookedAppointments) {
-            if (appointment.getDoctorName().equals(doctor.getName())) {
-                isAvailable = false;
-                break;
-            }
-        }
-
-        return isAvailable;
-    } // end method checkDoctorAvailability
-
-    /**
-     * checks if there is any doctor is available for a certain appointment time slot
-     * 
-     * @param date a LocalDate of the appointment date
-     * @param time a LocalTime of the appointment time
-     * @return a boolean indicating if there is any available doctor
-     */
-    static boolean checkTimeSlotAvailability(LocalDate date, LocalTime time) {
-        boolean isAvailable = false;
-        ArrayList<Appointment> bookedAppointments = getAppointmentsOfSameTime(date, time);
+    static ArrayList<Doctor> getSpecialtyDoctors(ArrayList<Object> doctorList, String specialty) {
+        ArrayList<Doctor> specialists = new ArrayList<>();
 
         for (Object obj : doctorList) {
             Doctor doctor = (Doctor) obj;
-            isAvailable = checkDoctorAvailability(doctor, bookedAppointments);
-            if (isAvailable) {
-                break;
+            if (doctor.getSpecialty().equals(specialty)) {
+                specialists.add(doctor);
             }
         } 
 
-        return isAvailable;
-    } // end method checkTimeSlotAvailability
+        return specialists;
+    } // end method getSpecialtyDoctors
+
+    /**
+     * gets all appointments for a certain doctor
+     * 
+     * @param appointmentList an ArrayList of Object containing all appointments
+     * @param doctor the doctor to check
+     * @return an ArrayList of Appointment for the specified doctor
+     */
+    static ArrayList<Appointment> getDoctorAppointments(ArrayList<Object> appointmentList, Doctor doctor) {
+        ArrayList<Appointment> doctorAppointments = new ArrayList<>();
+
+        for (Object obj : appointmentList) {
+            Appointment appointment = (Appointment) obj;
+            if (doctor.getName().equals(appointment.getDoctorName())) {
+                appointmentList.add(appointment);
+            }
+        } 
+
+        return doctorAppointments;
+    } // end method getDoctorAppointments 
+
+    /**
+     * gets a certain doctor's availability for a certain date and time
+     * 
+     * @param doctorAppointments an ArrayList of Appointments of a certain doctor
+     * @param date a LocalDate to check
+     * @param time a LocalTime to check
+     * @return a boolean indicating if the doctor is available
+     */
+    static boolean checkAvailability(ArrayList<Appointment> doctorAppointments, LocalDate date, LocalTime time) {
+        for (Object obj : doctorAppointments) {
+            Appointment appointment = (Appointment) obj;
+            if (appointment.getDate().equals(date) && appointment.getStartTime().equals(time)
+                    && appointment.getStatus().equals("Comfirmed")) {
+                return false;
+            }
+        }
+        return true;
+    } // end method checkAvailability
+
+    /**
+     * gets all incoming appointments
+     * 
+     * @param appointmentList an ArrayList of Object containing all appointments
+     * @return an ArrayList of Object containing the appointments from today and after
+     */
+    static ArrayList<Object> getFutureAppointments(ArrayList<Object> appointmentList) {
+        ArrayList<Object> futureAppointments = new ArrayList<>();
+
+        for (Object obj : appointmentList) {
+            Appointment appointment = (Appointment) obj;
+            if (!appointment.getDate().isBefore(LocalDate.now())) {
+                futureAppointments.add(obj);
+            }
+        } 
+
+        return futureAppointments;
+    } // end method getFutureAppointments
 } // end class ReceptionPage
