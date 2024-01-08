30e245d8d991005831a4f6d3d4da5f1fed57677d
Xintang ZHANG <xtzhang1994@gmail.com>
2024-01-08 16:57:28 -0500

    added table style to schedule

--- src/main/ReceptionPage.java
+++ src/main/ReceptionPage.java
@@ -29,6 +29,8 @@ public class ReceptionPage extends Team6MedicalClinic {
     private static ArrayList<Object> appointmentList = ObjectIO.loadData(ObjectIO.APPOINTMENT_FILE_PATH);
     private static ArrayList<Object> doctorList = ObjectIO.loadData(ObjectIO.DOCTOR_FILE_PATH);
 
+    private static ArrayList<Appointment> appointments = new ArrayList<>();
+
     public static void main(String[] args) {
         // all incoming appointments
         ArrayList<Object> futureAppointment = getFutureAppointments(appointmentList);
@@ -48,66 +50,70 @@ public class ReceptionPage extends Team6MedicalClinic {
         // TODO:
         // ask for specialty (call getSpecialtyDoctors
         // select doctor (doctor = chooseFromMenu
-        // display doctor schedule (printSchedule
-        printSchedule(scheduleStartDate, scheduleEndDate, null);
+        // display doctor schedule (printSchedule   
+        System.out.println(
+                "+---------------------------------------------------------------------------------------------+"); 
+        System.out.print("|                    "); 
+        printScheduleHeader(BusinessHours.OPEN_TIME_AM.getTime());   
+        printSchedule(scheduleStartDate, scheduleEndDate, appointments);
         // ask if to create an appointment
         // ask if the patient is new
         // search for patient || create new patient
         // select date
         // select time
         // enter the rest of attrs for the appointment
-        String patientName = "";
-        boolean noPatientDetails = true;
-        while (noPatientDetails) {
-            if (user.getResponse("Is the person a new patient?")) {
-                System.out.println("\n======= NEW PATIENT =======");
-                Patient patient = user.createPatient();
-                patientList.add(patient);
-                patientName = patient.getName();
-                noPatientDetails = false;
-                // ObjectIO.writeObjects(ObjectIO.PATIENT_FILE_PATH, patients);
-            } else {
-                Object patient = user.searchForPerson(patientList);
-                if (patient == null) {
-                    System.out.println("\nThis person does not exist.");
-                } else {
-                    patientName = ((Patient)patient).getName();
-                    noPatientDetails = false;
-                }
-            }
-        }
-        scanner.nextLine();
-        System.out.println("\n======= ENTER APPOINTMENT DETAILS =======");
-        Appointment appointment = UserInteraction.createAppointment(scanner, patientName);
-        System.out.println("\n======= APPOINTMENT SUCCESSFULLY CREATED =======");
-        System.out.println(appointment);
-        scanner.nextLine();
+        // String patientName = "";
+        // boolean noPatientDetails = true;
+        // while (noPatientDetails) {
+        //     if (user.getResponse("Is the person a new patient?")) {
+        //         System.out.println("\n======= NEW PATIENT =======");
+        //         Patient patient = user.createPatient();
+        //         patientList.add(patient);
+        //         patientName = patient.getName();
+        //         noPatientDetails = false;
+        //         // ObjectIO.writeObjects(ObjectIO.PATIENT_FILE_PATH, patients);
+        //     } else {
+        //         Object patient = user.searchForPerson(patientList);
+        //         if (patient == null) {
+        //             System.out.println("\nThis person does not exist.");
+        //         } else {
+        //             patientName = ((Patient)patient).getName();
+        //             noPatientDetails = false;
+        //         }
+        //     }
+        // }
+        // scanner.nextLine();
+        // System.out.println("\n======= ENTER APPOINTMENT DETAILS =======");
+        // Appointment appointment = UserInteraction.createAppointment(scanner, patientName);
+        // System.out.println("\n======= APPOINTMENT SUCCESSFULLY CREATED =======");
+        // System.out.println(appointment);
+        // scanner.nextLine();
     };
     
     // search appointments
     private static Runnable searchAppointments = () -> {
-        String[] filterMenu = {"Confirmed", "Cancelled"};
-        ArrayList<Object> filter = new ArrayList<>();
-        int response = UserInteraction.chooseFromMenu(scanner, filterMenu);
-        for (Object obj : appointmentList) {
-            if (((Appointment) obj).getStatus().equals(filterMenu[response - 1])) {
-                filter.add(obj);
-            }
-        }
-        ArrayList<Object> smallerFilter = UserInteraction.searchForAppointments(scanner, filter);
-        if (smallerFilter.size() == 0) {
-            System.out.println("\nThere are no appointments that matches the criteria.");
-        } else if (smallerFilter.size() == 1){
-            if (user.getResponse("There is only one appointment. Do you want to cancel it?")) {
-                ((Appointment)smallerFilter.get(0)).setStatus("Cancelled");
-                System.out.println("======= APPOINTMENT CANCELLED =======");
-                System.out.println((Appointment)smallerFilter.get(0));
-            }
-        } else {
-            for (Object obj : smallerFilter) {
-                System.out.println((Appointment) obj);
-            }
-        }
+        // String[] filterMenu = {"Confirmed", "Cancelled"};
+        // ArrayList<Object> filter = new ArrayList<>();
+        // int response = UserInteraction.chooseFromMenu(scanner, filterMenu);
+        // for (Object obj : appointmentList) {
+        //     if (((Appointment) obj).getStatus().equals(filterMenu[response - 1])) {
+        //         filter.add(obj);
+        //     }
+        // }
+        // ArrayList<Object> smallerFilter = UserInteraction.searchForAppointments(scanner, filter);
+        // if (smallerFilter.size() == 0) {
+        //     System.out.println("\nThere are no appointments that matches the criteria.");
+        // } else if (smallerFilter.size() == 1){
+        //     if (user.getResponse("There is only one appointment. Do you want to cancel it?")) {
+        //         ((Appointment)smallerFilter.get(0)).setStatus("Cancelled");
+        //         System.out.println("======= APPOINTMENT CANCELLED =======");
+        //         System.out.println((Appointment)smallerFilter.get(0));
+        //     }
+        // } else {
+        //     for (Object obj : smallerFilter) {
+        //         System.out.println((Appointment) obj);
+        //     }
+        // }
     };
 
     // display all appointments
@@ -140,45 +146,72 @@ public class ReceptionPage extends Team6MedicalClinic {
      * @param time a LocalTime when the time table begins
      */
     static void printDailySchedule(LocalDate date, LocalTime time, ArrayList<Appointment> doctorAppointments) {
-        if (time.equals(BusinessHours.CLOSE_TIME_PM.getTime())) {   // base case
+        if (time.equals(BusinessHours.CLOSE_TIME_PM.getTime())) { // base case
             System.out.println("|");
             return;
         }
 
         if (time.equals(BusinessHours.CLOSE_TIME_AM.getTime())) {
-            System.out.print("\t");
+            System.out.print("|");
             printDailySchedule(date, BusinessHours.OPEN_TIME_PM.getTime(), doctorAppointments); // skip lunch break
         } else {
             if (checkAvailability(doctorAppointments, date, time)) {
-                System.out.printf("%s\t", time);    // if the doctor is available, print the time slot
+                System.out.print("|           ");    // if the doctor is available, print nothing
                 printDailySchedule(date, time.plusMinutes(60), doctorAppointments);
             } else {
-                System.out.printf("\t\t");      // if the doctor is not available, print white spaces only
+                System.out.print("|     x     ");      // if the doctor is not available, print x
                 printDailySchedule(date, time.plusMinutes(60), doctorAppointments);
             }
         }
     } // end method printDailySchedule
 
     /**
-     * recursively prints a schedule of a specified range of days
+     * recursively prints a schedule table of a specified range of days
      * 
      * @param startDate a LocalDate of the date the schedule begins
      * @param endDate a LocalDate of the date the schedule ends
      */
     static void printSchedule(LocalDate startDate, LocalDate endDate, ArrayList<Appointment> doctorAppointments) {
-        if (startDate.isAfter(endDate)) {   // base case
+        System.out.println(
+                "+---------------------------------------------------------------------------------------------+");
+        if (startDate.isAfter(endDate)) { // base case
             return;
         }
 
         if (startDate.getDayOfWeek().equals(DayOfWeek.SATURDAY) || startDate.getDayOfWeek().equals(DayOfWeek.SUNDAY)) { // skip weekends
-            System.out.println("|");
-            printSchedule(startDate.plusDays(1), endDate, doctorAppointments);
+            System.out.println(
+                    "|                                                                                             |");
+            printSchedule(startDate.plusDays(2), endDate, doctorAppointments);
         } else {
-            System.out.printf("| %s (%s)\t", startDate.getDayOfWeek().toString().substring(0, 3), startDate);     // print day of the week and date
+            System.out.printf("|  %s (%s)  ", startDate.getDayOfWeek().toString().substring(0, 3), startDate);     // print day of the week and date
             printDailySchedule(startDate, BusinessHours.OPEN_TIME_AM.getTime(), doctorAppointments);    // print the time slots for the day
             printSchedule(startDate.plusDays(1), endDate, doctorAppointments);
         }
     } // end method printSchedule
+
+    /**
+     * recursively prints a time table of a day.
+     * each time slot is of 30-min length, 
+     * the time table will be from 9am to 14pm, 
+     * with a lunchbreak from noon to 1pm
+     * 
+     * @param date a LocalDate of the current day
+     * @param time a LocalTime when the time table begins
+     */
+    static void printScheduleHeader(LocalTime time) {
+        if (time.equals(BusinessHours.CLOSE_TIME_PM.getTime())) { // base case
+            System.out.println("|");
+            return;
+        }
+
+        if (time.equals(BusinessHours.CLOSE_TIME_AM.getTime())) {
+            System.out.print("|");
+            printScheduleHeader(BusinessHours.OPEN_TIME_PM.getTime()); // skip lunch break
+        } else {
+            System.out.printf("|   %s   ", time);      // if the doctor is not available, print x
+            printScheduleHeader(time.plusMinutes(60));
+        }
+    } // end method printScheduleHeader    
 
     /**
      * gets all doctors of a certain specialty
